c92408523811e0c2f5201339ef31b5fbade90caf
shreejan shrestha <shr.shreejan@gmail.com>
2024-11-08 22:49:34 +0545

    Added changes to include file name and linked details on resource obj

--- force-app/main/default/lwc/resourceUploadFile/resourceUploadFile.js
+++ force-app/main/default/lwc/resourceUploadFile/resourceUploadFile.js
@@ -1,4 +1,6 @@
 import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import commitUploadChanges from '@salesforce/apex/Resource.commitUploadChanges';
 
 export default class ResourceUploadFile extends LightningElement {
     @api 
@@ -9,6 +11,24 @@ export default class ResourceUploadFile extends LightningElement {
     }
 
     handleUpload(event) {
-        console.log(event.detail.files);
+        commitUploadChanges({
+            recordId: this.recordId, 
+            fileName: event.detail.files[0].name
+        })
+        .then(() => {
+            this.showToast('Success', 'File uploaded successfully.', 'success');
+        })
+        .catch((error) => {
+            this.showToast('Error', error.body.message || 'An error occurred while uploading the file.', 'error');
+        });
+    }
+
+    showToast(title, message, variant) {
+        const event = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+        });
+        this.dispatchEvent(event);
     }
 }
